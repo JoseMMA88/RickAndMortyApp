dfd751f314d2919fb7a1f90d36123c7f8544b97f
Jose Manuel Malagón Alba <contact@josemalagon.es>
2023-12-17 17:00:02 +0100

    Initial Commit

---
+++ RickAndMortyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyApp
+//
+//  Created by Jose Manuel Malagón Alba on 17/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
